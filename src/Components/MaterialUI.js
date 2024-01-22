1d91358cc0a2e56734d211887ec730857cbe11c5
Eddie Pina <eddiepina@Eddies-MacBook-Pro.local>
2024-01-22 16:34:05 -0400

    feat: Material UI and React Bootstrap

---
+++ src/Components/MaterialUI.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MediaCard from "./MaterialCard";
+import Drawer from "./Drawer";
+
+export default function MaterialUI() {
+  return (
+    <>
+      <h2>MaterialUI</h2>
+      <MediaCard/>
+    </>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Bootstrap from '../Components/Bootstrap';
+import ReactBootstrap from '../Components/ReactBootstrap';
+import MaterialUI from '@/Components/MaterialUI';
 
 export default function Home() {
   return (
     <div>
       <h1>CSS Frameworks with React.</h1>
-      <Bootstrap/>
+      <MaterialUI/>
     </div>
   );
 }
